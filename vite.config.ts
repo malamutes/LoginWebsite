88d13c5775521f5d5464bdfcee3f2b06e0cf2bc4
malamutes <hehexdroflmfao@gmail.com>
2024-10-31 23:56:51 +0800

    MoreEdits

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches your folder structure
+  },
 })
